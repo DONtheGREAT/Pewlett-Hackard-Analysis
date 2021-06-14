3f3cf7906efcbb1574470172aba88472a4b3815e
Donald Graham <donaldg1994@yahoo.com>
2021-06-13 20:32:30 -0400

    queries added

--- schema.sql
+++ schema.sql
@@ -42,7 +42,11 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	-- PRIMARY KEY (emp_no)
+  --Primary key had to be removed from titles because emp_no column had duplicates due to employees having worked multiple positions since being with the company.
+  --Therefore the titles table will not used as a primary key for any other table because it does not have unique values.
+  --The table had to be removed and created again becaue SQL doesnt allow for duplicates or overrides. THANKFULLY!
+  --DROP TABLE 'table_name' CASCADE; cascade inidicates remove the connections to other tables in database.
 );
 CREATE TABLE dept_employees (
 	emp_no INT NOT NULL,
@@ -57,3 +61,49 @@ CREATE TABLE dept_employees (
 --asterisk tells Postgres that we're looking for every column in a table.
 --FROM departments tells pgAdmin which table to search
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM dept_employees;
+SELECT * FROM titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+--The into function tells postgres to select this data and insert it into a new table.
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
